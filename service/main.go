1f9e67e8ac415304b36c06bab8a9b3fa983fdc96
Ju Pan <jupan1994@gmail.com>
2017-12-10 22:56:23 -0700

    Add signup and signin backend code

--- service/main.go
+++ service/main.go
@@ -13,6 +13,9 @@ import (
   "context"
   "cloud.google.com/go/storage"
   "io"
+  "github.com/auth0/go-jwt-middleware"
+  "github.com/dgrijalva/jwt-go"
+  "github.com/gorilla/mux"
 )
 
 type Location struct {
@@ -38,6 +41,8 @@ const (
   // Needs to update this bucket based on your gcs bucket name.
   BUCKET_NAME = "post-images-185721"
 )
+
+var mySigningKey = []byte("secret")
 
 func main() {
   // Create a client
@@ -72,8 +77,26 @@ func main() {
     }
   }
   fmt.Println("started-service")
-  http.HandleFunc("/post", handlerPost)
-  http.HandleFunc("/search", handlerSearch)
+
+  /*
+    Following code is for Authentication (Token)
+  */
+  // Instantiate the gorilla/mux router
+  r := mux.NewRouter()
+
+  var jwtMiddleware = jwtmiddleware.New(jwtmiddleware.Options{
+    ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
+      return mySigningKey, nil
+    },
+    SigningMethod: jwt.SigningMethodHS256,
+  })
+
+  r.Handle("/post", jwtMiddleware.Handler(http.HandlerFunc(handlerPost))).Methods("POST")
+  r.Handle("/search", jwtMiddleware.Handler(http.HandlerFunc(handlerSearch))).Methods("GET")
+  r.Handle("/login", http.HandlerFunc(loginHandler)).Methods("POST")
+  r.Handle("/signup", http.HandlerFunc(signupHandler)).Methods("POST")
+
+  http.Handle("/", r)
   log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
@@ -144,6 +167,10 @@ func handlerPost(w http.ResponseWriter, r *http.Request) {
   w.Header().Set("Access-Control-Allow-Origin", "*")
   w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
 
+  user := r.Context().Value("user")
+  claims := user.(*jwt.Token).Claims
+  username := claims.(jwt.MapClaims)["username"]
+
   // 32 << 20 is the maxMemory param for ParseMultipartForm, equals to 32MB (1MB = 1024 * 1024 bytes = 2^20 bytes)
   // After you call ParseMultipartForm, the file will be saved in the server memory with maxMemory size.
   // If the file size is larger than maxMemory, the rest of the data will be saved in a system temporary file.
@@ -154,7 +181,7 @@ func handlerPost(w http.ResponseWriter, r *http.Request) {
   lat, _ := strconv.ParseFloat(r.FormValue("lat"), 64)
   lon, _ := strconv.ParseFloat(r.FormValue("lon"), 64)
   p := &Post{
-    User: "1111",
+    User: username.(string),
     Message: r.FormValue("message"),
     Location: Location{
       Lat: lat,
---
+++ service/user.go
@@ -0,0 +1,161 @@
+package main
+
+import (
+  elastic "gopkg.in/olivere/elastic.v3"
+
+  "encoding/json"
+  "fmt"
+  "net/http"
+  "reflect"
+  "time"
+
+  "github.com/dgrijalva/jwt-go"
+)
+
+const (
+  TYPE_USER = "user"
+)
+
+type User struct {
+  Username string `json:"username"`
+  Password string `json:"password"`
+}
+
+// checkUser checks whether a user is valid
+func checkUser(username, password string) bool {
+  es_client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
+  if err != nil {
+    fmt.Printf("ES is not setup %v\n", err)
+    return false
+  }
+
+  // Search with a term query
+  termQuery := elastic.NewTermQuery("username", username)
+  queryResult, err := es_client.Search().
+      Index(INDEX).
+      Query(termQuery).
+      Pretty(true).
+      Do()
+  if err != nil {
+    fmt.Printf("ES query failed %v\n", err)
+    return false
+  }
+
+  var tyu User
+  for _, item := range queryResult.Each(reflect.TypeOf(tyu)) {
+    u := item.(User)
+    return u.Password == password && u.Username == username
+  }
+  // If there is no user existing, return false
+  return false
+}
+
+// Add a new user. Return true if successful
+func addUser(username, password string) bool {
+  // We usually use bigtable, but bigtable is more expensive than ES, we use ES.
+  es_client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
+  if err != nil {
+    fmt.Printf("ES is not setup %v\n", err)
+    return false
+  }
+
+  user := &User {
+    Username: username,
+    Password: password,
+  }
+
+  // Search with a term query
+  termQuery := elastic.NewTermQuery("username", username)
+  queryResult, err := es_client.Search().
+      Index(INDEX).
+      Query(termQuery).
+      Pretty(true).
+      Do()
+  if err != nil {
+    fmt.Printf("ES query failed %v\n", err)
+    return false
+  }
+
+  if queryResult.TotalHits() > 0 {
+    fmt.Printf("User %s exists, cannot create duplicate user.\n", username)
+    return false
+  }
+
+  // Save it to index
+  _, err = (es_client.Index().
+      Index(INDEX)).
+      Type(TYPE_USER).
+      Id(username).
+      BodyJson(user).
+      Refresh(true).
+      Do()
+  if err != nil {
+    fmt.Printf("ES save failed %v\n", err)
+    return false
+  }
+  return true
+}
+
+// If signup is successful, a new session is created.
+func signupHandler(w http.ResponseWriter, r *http.Request) {
+  fmt.Println("Received one signup request.")
+
+  // Decode a user from request (POST)
+  decoder := json.NewDecoder(r.Body)
+  var u User
+  if err := decoder.Decode(&u); err != nil {
+    panic(err)
+    return
+  }
+
+  // Check whether username and password are empty, if any of them is empty,
+  // call http.Error(w, "Empty password or username", http.StatusInternalServerError)
+  if u.Username != "" && u.Password != "" {
+    if addUser(u.Username, u.Password) {
+      fmt.Println("User added successfully")
+      w.Write([]byte("User added successfully"))
+    } else {
+      fmt.Println("Failed to add a new user.")
+      http.Error(w, "Failed to add a new user.", http.StatusInternalServerError)
+    }
+  } else {
+    fmt.Println("Empty password or username")
+    http.Error(w, "Empty password or username", http.StatusInternalServerError)
+  }
+
+  w.Header().Set("Content-Type", "text/plain")
+  w.Header().Set("Access-Control-Allow-Origin", "*")
+}
+
+// If login is successful, a new token is created.
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+  fmt.Println("Received one login request.")
+
+  decoder := json.NewDecoder(r.Body);
+  var u User
+  if err := decoder.Decode(&u); err != nil {
+    panic(err)
+    return
+  }
+
+  if checkUser(u.Username, u.Password) {
+    token := jwt.New(jwt.SigningMethodHS256)                  // Create a new token object to store.
+    claims := token.Claims.(jwt.MapClaims)                    // Convert it into a map for lookup
+
+    // Set token claims
+    claims["username"] = u.Username                           // Store username into it.
+    claims["exp"] = time.Now().Add(time.Hour * 24).Unix()     // Store expiration into it.
+
+    // Sign the token with our secret(private key)
+    tokenString, _ := token.SignedString(mySigningKey)        // Sign (Encrypt) and token such that only server knows it.
+
+    // Finally, write the token to the browser window
+    w.Write([]byte(tokenString))                              // Write it into response
+  } else {
+    fmt.Println("Invalid password or username.")
+    http.Error(w, "Invalid password or username", http.StatusInternalServerError)
+  }
+
+  w.Header().Set("Content-Type", "text/plain")
+  w.Header().Set("Access-Control-Allow-Origin", "*")
+}
