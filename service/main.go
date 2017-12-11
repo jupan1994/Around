4c683d9780ce2939148e125af10c26150b274690
Ju Pan <jupan1994@gmail.com>
2017-12-10 23:47:57 -0700

    change gcp ip

--- service/main.go
+++ service/main.go
@@ -37,7 +37,7 @@ const (
   //PROJECT_ID  = "around-179500"
   //BT_INSTANCE = "around-post"
   // Needs to update this URL if you deploy it to cloud.
-  ES_URL = "http://35.227.113.252:9200"
+  ES_URL = "http://35.227.62.162:9200"
   // Needs to update this bucket based on your gcs bucket name.
   BUCKET_NAME = "post-images-185721"
 )
